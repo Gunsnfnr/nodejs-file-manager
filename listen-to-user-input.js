bfbc78aac1ce31e6432635b1ffab2787db5b4c78
Gunsnfnr <a.j.p.010203@gmail.com>
2024-10-12 16:18:34 +0300

    refactor: change if-else to switch

--- listen-to-user-input.js
+++ listen-to-user-input.js
@@ -3,16 +3,20 @@ import { changeDirectory } from "./src/change-directory.js";
 
 const listenToUserInput = (username) => {
   process.stdin.on("data", (chunk) => {
-    if (chunk.toString() === `.exit${os.EOL}`) {
-      console.log(`Thank you for using File Manager, ${username}, goodbye!`);
-      process.exit();
-    } else if (chunk.toString() === `up${os.EOL}`) {
-      process.chdir("..");
-      console.log(`You are currently in ${process.cwd()}`);
-    } else if (chunk.toString().startsWith("cd ")) {
-      changeDirectory(chunk);
-    } else {
-      console.error(`Invalid input`);
+    switch (true) {
+      case chunk.toString() === `.exit${os.EOL}`:
+        console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+        process.exit();
+        break;
+      case chunk.toString() === `up${os.EOL}`:
+        process.chdir("..");
+        console.log(`You are currently in ${process.cwd()}`);
+        break;
+      case chunk.toString().startsWith("cd "):
+        changeDirectory(chunk);
+        break;
+      default:
+        console.error(`Invalid input`);
     }
   });
 };
